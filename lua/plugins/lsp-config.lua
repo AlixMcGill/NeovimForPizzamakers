bbd80b1a0468c66e7e7c35ab5c64083055fcb979
AlixMcGill <alixfmcgill@gmail.com>
2025-11-03 00:02:34 -0600

    Updated lsp-config for 0.11

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -14,7 +14,7 @@ return {
             "ts_ls",
             --"csharp_ls",
             "cssls",
-            --"eslint", 
+            "eslint",
             "html",
             "omnisharp",
             "clangd",
@@ -24,50 +24,45 @@ return {
       })
     end
   },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      local util = require("lspconfig/util")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    {
+      "neovim/nvim-lspconfig",
+      config = function()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        local util = require("lspconfig/util")
 
-      local on_attach = function(client, bufnr)
-          local opts = {buffer = bufnr, noremap = true, silent = true}
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-          vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-      end
+        local on_attach = function(client, bufnr)
+          local opts = { buffer = bufnr, noremap = true, silent = true }
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+        end
 
-
-      lspconfig.lua_ls.setup({capabilities = capabilities})
-      lspconfig.ts_ls.setup({capabilities = capabilities})
-      --lspconfig.csharp_ls.setup({capabilities = capabilities})
-      lspconfig.cssls.setup({ capabilities = capabilities})
-      lspconfig.gopls.setup({
+        vim.lsp.config("lua_ls", { capabilities = capabilities })
+        vim.lsp.config("ts_ls", { capabilities = capabilities })
+        vim.lsp.config("cssls", { capabilities = capabilities })
+        vim.lsp.config("eslint", { capabilities = capabilities })
+        vim.lsp.config("html", { capabilities = capabilities })
+        vim.lsp.config("omnisharp", { capabilities = capabilities })
+        vim.lsp.config("clangd", {
+          capabilities = capabilities,
+          cmd = { "clangd" },
+          filetypes = { "c", "cpp", "objc", "objcpp" },
+        })
+        vim.lsp.config("intelephense", {
+          capabilities = capabilities,
+          on_attach = function(client)
+            if client.name == "intelephense" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end,
+        })
+        vim.lsp.config("gopls", {
           on_attach = on_attach,
           capabilities = capabilities,
-          cmd = {"gopls"},
-          filetypes = {"go", "gomod", "gowork", "gotmpl" },
-          root_dir = util.root_pattern("gowork", "go.mod", ".git"),
-      })
-      --lspconfig.eslint.setup({})
-      lspconfig.html.setup({capabilities = capabilities})
-      lspconfig.omnisharp.setup({capabilities = capabilities})
-      --lspconfig.harper_ls.setup({capabilities = capabilities})
-      lspconfig.clangd.setup({
-            capabilities = capabilities,
-          cmd = { "clangd" },
-          filetypes = {"c", "cpp", "objc", "objcpp"},
-      })
-      lspconfig.intelephense.setup({
-          capabilities = capabilities,
-          on_attach = function (client)
-              if client.name == "intelephense" then
-                  client.server_capabilities.documentFormattingProvider = false
-              end
-          end
-      })
-
-    end
-  }
+          cmd = { "gopls" },
+          filetypes = { "go", "gomod", "gowork", "gotmpl" },
+          root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+        })
+      end,
+    }
 }
